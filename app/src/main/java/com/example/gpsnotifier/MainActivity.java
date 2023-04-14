8ef79067d092b683625f99993bb0cae92fc5af6f
Joel-mq <joel.woodhouse@students.mq.edu.au>
2023-04-14 16:52:45 +1000

    weird error? maybe i did something wrong

--- app/src/main/java/com/example/gpsnotifier/MainActivity.java
+++ app/src/main/java/com/example/gpsnotifier/MainActivity.java
@@ -14,9 +14,6 @@ public class MainActivity extends AppCompatActivity {
     private String testString = "test";
 
 
-public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
