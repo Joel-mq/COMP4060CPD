60e61404b41322c02c432dcf01a46c36086fba8c
Joel-mq <joel.woodhouse@students.mq.edu.au>
2023-04-14 16:45:54 +1000

    Working button

--- app/src/main/java/com/example/gpsnotifier/MainActivity.java
+++ app/src/main/java/com/example/gpsnotifier/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.gpsnotifier;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private TextView test;
+    private String testString = "test";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        test = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                test.setText(testString);
+            }
+        });
     }
 }
