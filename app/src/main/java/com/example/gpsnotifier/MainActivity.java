a6cf606a4aab22a2013db6330fcc29c54ff87510
Joel-mq <joel.woodhouse@students.mq.edu.au>
2023-04-26 17:19:49 +1000

    Attempt at notification
    Not sure if this will work

--- app/src/main/java/com/example/gpsnotifier/MainActivity.java
+++ app/src/main/java/com/example/gpsnotifier/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.gpsnotifier;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "testID";
     private Button button;
     private TextView test;
     private String testString = "test";
@@ -21,16 +30,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
 
         button = findViewById(R.id.button);
         test = findViewById(R.id.textView);
 
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Test notification")
+                .setContentText("This is a test notification")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View v) {
+                notificationManager.notify(0, builder.build());
                 test.setText(testString);
             }
         });
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "test name";
+            String description = "test desc";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
